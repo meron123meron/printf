9e8f5db381216a661634e01c3d2a937e9219eae7
meron <meron@example.com>
2022-07-12 08:46:48 -0700

    task 2 trial

--- printf.c
+++ printf.c
@@ -11,9 +11,23 @@
 int _printf(const char *format, ...)
 {
 va_list args;
-int we;
+int we, a[12], x, i = 0, n;
 va_start(args, format);
 we = vfprintf(stdout, format, args);
+if (format[i] == 'b')
+{
+int y = va_arg(args, int);
+for (x = 0; n > 0; x++)
+{
+a[x] = n % 2;
+n = n / 2;
+}
+for (x = x -1; x >= 0; x++)
+{
+y = a[x];
+return (y);
+}
+}
 va_end(args);
 return (we);
 }
